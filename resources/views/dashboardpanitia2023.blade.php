6b79b3e919ffdd7bf8fa8829058473c39c0fdbe4
ToastedBaguette <ronyhartonoirawan@gmail.com>
2023-08-09 14:08:18 +0700

    update frontend

--- resources/views/dashboardpanitia2023.blade.php
+++ resources/views/dashboardpanitia2023.blade.php
@@ -100,13 +100,13 @@
         </a>
     </li>
     <!-- <li class="menu-item " aria-haspopup="true">
-        <a href="{{ url('listkendala') }}" class="menu-link">
-            <i class="menu-bullet menu-bullet-dot">
-                <span></span>
-            </i>
-            <span class="menu-text">Bukti Kendala</span>
-        </a>
-    </li> -->
+            <a href="{{ url('listkendala') }}" class="menu-link">
+                <i class="menu-bullet menu-bullet-dot">
+                    <span></span>
+                </i>
+                <span class="menu-text">Bukti Kendala</span>
+            </a>
+        </li> -->
 @endsection
 
 @section('naved')
@@ -323,11 +323,18 @@
                                         </figure>
                                     @endif
                                     @if (Auth::user()->divisi == 'BPH')
-                                    <figure>
-                                        <img class="img-fluid floating img-change-bph"
-                                            src="{{ url('./assets/mob-assets/dodot.png') }}" 2x" alt=""
-                                            style="margin-left: 0" />
-                                    </figure>
+                                        <figure>
+                                            <img class="img-fluid floating img-change-bph"
+                                                src="{{ url('./assets/mob-assets/dodot.png') }}" 2x" alt=""
+                                                style="margin-left: 0" />
+                                        </figure>
+                                    @endif
+                                    @if (Auth::user()->divisi == 'ITD')
+                                        <figure>
+                                            <img class="img-fluid floating img-change-itd"
+                                                src="{{ url('./assets/mob-assets/roni1.jpg') }}" 2x" alt=""
+                                                style="margin-left: 0" />
+                                        </figure>
                                     @endif
                                 </div>
                                 <!--end::User-->
@@ -339,15 +346,15 @@
                     <!-- <div class="card card-custom bg-danger card-stretch gutter-b"> -->
                     <!--begin::Header-->
                     <!-- <div class="card-body border-0 py-5">
-                            <h1 class="font-weight-bolder text-white" style="margin-bottom: 0.5vw;">{{ Auth::user()->name }}</h1>
-                            <h2 class="text-white" style="font-weight: normal; font-size: 100%;">{{ Auth::user()->nrp }}</h2>
-                            <h2 class="text-white" style="font-weight: normal; font-size: 100%;">{{ Auth::user()->email }}</h2>
-                            @if (Auth::user()->divisi == 'MAPING')
+                                <h1 class="font-weight-bolder text-white" style="margin-bottom: 0.5vw;">{{ Auth::user()->name }}</h1>
+                                <h2 class="text-white" style="font-weight: normal; font-size: 100%;">{{ Auth::user()->nrp }}</h2>
+                                <h2 class="text-white" style="font-weight: normal; font-size: 100%;">{{ Auth::user()->email }}</h2>
+                                @if (Auth::user()->divisi == 'MAPING')
     <h2 class="text-white" style="font-weight: normal; font-size: 100%;">Kelompok Delta : {{ Auth::user()->alpha }}</h2>
-                            <h2 class="text-white" style="font-weight: normal; font-size: 100%;">Kelompok Echo : {{ Auth::user()->beta }}</h2>
+                                <h2 class="text-white" style="font-weight: normal; font-size: 100%;">Kelompok Echo : {{ Auth::user()->beta }}</h2>
     @endif
-                        </div>
-                    </div> -->
+                            </div>
+                        </div> -->
                     <!--end::Mixed Widget 1-->
 
                     <!-- <div class="col-xxl-8 order-2 order-xxl-1"> -->
@@ -355,22 +362,22 @@
                     <!-- <div class="card card-custom card-stretch gutter-b"> -->
                     <!--begin::Header-->
                     <!-- <div class="card-header align-items-center border-0 mt-4">
-                                <h3 class="card-title align-items-start flex-column">
-                                    <span class="font-weight-bolder text-dark">Rundown</span>
+                                    <h3 class="card-title align-items-start flex-column">
+                                        <span class="font-weight-bolder text-dark">Rundown</span>
 
-                                    @if (count($listrundown) > 0)
+                                        @if (count($listrundown) > 0)
     <span class="text-muted mt-3 font-weight-bold font-size-sm">  <?php //echo date('d F, Y', strtotime($listrundown[0]->tanggal));
     ?></span>
     @endif
-                                </h3>
-                            </div> -->
+                                    </h3>
+                                </div> -->
                     <!--end::Header-->
                     <!--begin::Body-->
                     <!-- <div class="card-body pt-4">
-                                <div class="timeline timeline-5">
+                                    <div class="timeline timeline-5">
 
-                                    <div class="timeline-items">
-                                        @foreach ($listrundown as $r)
+                                        <div class="timeline-items">
+                                            @foreach ($listrundown as $r)
     <?php //$rd_awal = date('H:i', strtotime($r->waktu_awal));
     //$rd_akhir = date('H:i', strtotime($r->waktu_akhir));
     //if (date("H:i") > $rd_akhir) {
@@ -379,27 +386,27 @@
                     <!-- <div class="timeline-item"> -->
                     <!--begin::Icon-->
                     <!-- <div class="timeline-media bg-light-success">
-                                                    <span class="svg-icon svg-icon-success svg-icon-2x"> -->
+                                                        <span class="svg-icon svg-icon-success svg-icon-2x"> -->
                     <!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo1/dist/../src/media/svg/icons/Navigation/Check.svg-->
                     <!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                                <polygon points="0 0 24 0 24 24 0 24" />
-                                                                <path d="M6.26193932,17.6476484 C5.90425297,18.0684559 5.27315905,18.1196257 4.85235158,17.7619393 C4.43154411,17.404253 4.38037434,16.773159 4.73806068,16.3523516 L13.2380607,6.35235158 C13.6013618,5.92493855 14.2451015,5.87991302 14.6643638,6.25259068 L19.1643638,10.2525907 C19.5771466,10.6195087 19.6143273,11.2515811 19.2474093,11.6643638 C18.8804913,12.0771466 18.2484189,12.1143273 17.8356362,11.7474093 L14.0997854,8.42665306 L6.26193932,17.6476484 Z" fill="#000000" fill-rule="nonzero" transform="translate(11.999995, 12.000002) rotate(-180.000000) translate(-11.999995, -12.000002) " />
-                                                            </g>
-                                                        </svg> -->
+                                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                                    <polygon points="0 0 24 0 24 24 0 24" />
+                                                                    <path d="M6.26193932,17.6476484 C5.90425297,18.0684559 5.27315905,18.1196257 4.85235158,17.7619393 C4.43154411,17.404253 4.38037434,16.773159 4.73806068,16.3523516 L13.2380607,6.35235158 C13.6013618,5.92493855 14.2451015,5.87991302 14.6643638,6.25259068 L19.1643638,10.2525907 C19.5771466,10.6195087 19.6143273,11.2515811 19.2474093,11.6643638 C18.8804913,12.0771466 18.2484189,12.1143273 17.8356362,11.7474093 L14.0997854,8.42665306 L6.26193932,17.6476484 Z" fill="#000000" fill-rule="nonzero" transform="translate(11.999995, 12.000002) rotate(-180.000000) translate(-11.999995, -12.000002) " />
+                                                                </g>
+                                                            </svg> -->
                     <!--end::Svg Icon-->
                     <!-- </span>
-                                                </div> -->
+                                                    </div> -->
                     <!--end::Icon-->
 
                     <!--begin::Info-->
                     <!-- <div class="timeline-desc timeline-desc-light-success">
-                                                    <span class="font-weight-bolder text-success"><?php //echo $rd_awal . "-" . $rd_akhir . " WIB";
-                                                    ?></span>
-                                                    <p class="font-weight-normal text-dark-50 pt-1 pb-2">
-                                                        {{ $r->kegiatan }}
-                                                    </p>
-                                                </div> -->
+                                                        <span class="font-weight-bolder text-success"><?php //echo $rd_awal . "-" . $rd_akhir . " WIB";
+                                                        ?></span>
+                                                        <p class="font-weight-normal text-dark-50 pt-1 pb-2">
+                                                            {{ $r->kegiatan }}
+                                                        </p>
+                                                    </div> -->
                     <!--end::Info-->
                     <!-- </div> -->
                     <!--end::Item-->
@@ -409,27 +416,27 @@
                     <!-- <div class="timeline-item"> -->
                     <!--begin::Icon-->
                     <!-- <div class="timeline-media bg-light-primary">
-                                                    <span class="svg-icon svg-icon-primary svg-icon-2x"> -->
+                                                        <span class="svg-icon svg-icon-primary svg-icon-2x"> -->
                     <!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo1/dist/../src/media/svg/icons/Navigation/Waiting.svg-->
                     <!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                                <rect x="0" y="0" width="24" height="24" />
-                                                                <path d="M19.5,10.5 L21,10.5 C21.8284271,10.5 22.5,11.1715729 22.5,12 C22.5,12.8284271 21.8284271,13.5 21,13.5 L19.5,13.5 C18.6715729,13.5 18,12.8284271 18,12 C18,11.1715729 18.6715729,10.5 19.5,10.5 Z M16.0606602,5.87132034 L17.1213203,4.81066017 C17.7071068,4.22487373 18.6568542,4.22487373 19.2426407,4.81066017 C19.8284271,5.39644661 19.8284271,6.34619408 19.2426407,6.93198052 L18.1819805,7.99264069 C17.5961941,8.57842712 16.6464466,8.57842712 16.0606602,7.99264069 C15.4748737,7.40685425 15.4748737,6.45710678 16.0606602,5.87132034 Z M16.0606602,18.1819805 C15.4748737,17.5961941 15.4748737,16.6464466 16.0606602,16.0606602 C16.6464466,15.4748737 17.5961941,15.4748737 18.1819805,16.0606602 L19.2426407,17.1213203 C19.8284271,17.7071068 19.8284271,18.6568542 19.2426407,19.2426407 C18.6568542,19.8284271 17.7071068,19.8284271 17.1213203,19.2426407 L16.0606602,18.1819805 Z M3,10.5 L4.5,10.5 C5.32842712,10.5 6,11.1715729 6,12 C6,12.8284271 5.32842712,13.5 4.5,13.5 L3,13.5 C2.17157288,13.5 1.5,12.8284271 1.5,12 C1.5,11.1715729 2.17157288,10.5 3,10.5 Z M12,1.5 C12.8284271,1.5 13.5,2.17157288 13.5,3 L13.5,4.5 C13.5,5.32842712 12.8284271,6 12,6 C11.1715729,6 10.5,5.32842712 10.5,4.5 L10.5,3 C10.5,2.17157288 11.1715729,1.5 12,1.5 Z M12,18 C12.8284271,18 13.5,18.6715729 13.5,19.5 L13.5,21 C13.5,21.8284271 12.8284271,22.5 12,22.5 C11.1715729,22.5 10.5,21.8284271 10.5,21 L10.5,19.5 C10.5,18.6715729 11.1715729,18 12,18 Z M4.81066017,4.81066017 C5.39644661,4.22487373 6.34619408,4.22487373 6.93198052,4.81066017 L7.99264069,5.87132034 C8.57842712,6.45710678 8.57842712,7.40685425 7.99264069,7.99264069 C7.40685425,8.57842712 6.45710678,8.57842712 5.87132034,7.99264069 L4.81066017,6.93198052 C4.22487373,6.34619408 4.22487373,5.39644661 4.81066017,4.81066017 Z M4.81066017,19.2426407 C4.22487373,18.6568542 4.22487373,17.7071068 4.81066017,17.1213203 L5.87132034,16.0606602 C6.45710678,15.4748737 7.40685425,15.4748737 7.99264069,16.0606602 C8.57842712,16.6464466 8.57842712,17.5961941 7.99264069,18.1819805 L6.93198052,19.2426407 C6.34619408,19.8284271 5.39644661,19.8284271 4.81066017,19.2426407 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
-                                                            </g>
-                                                        </svg> -->
+                                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                                    <rect x="0" y="0" width="24" height="24" />
+                                                                    <path d="M19.5,10.5 L21,10.5 C21.8284271,10.5 22.5,11.1715729 22.5,12 C22.5,12.8284271 21.8284271,13.5 21,13.5 L19.5,13.5 C18.6715729,13.5 18,12.8284271 18,12 C18,11.1715729 18.6715729,10.5 19.5,10.5 Z M16.0606602,5.87132034 L17.1213203,4.81066017 C17.7071068,4.22487373 18.6568542,4.22487373 19.2426407,4.81066017 C19.8284271,5.39644661 19.8284271,6.34619408 19.2426407,6.93198052 L18.1819805,7.99264069 C17.5961941,8.57842712 16.6464466,8.57842712 16.0606602,7.99264069 C15.4748737,7.40685425 15.4748737,6.45710678 16.0606602,5.87132034 Z M16.0606602,18.1819805 C15.4748737,17.5961941 15.4748737,16.6464466 16.0606602,16.0606602 C16.6464466,15.4748737 17.5961941,15.4748737 18.1819805,16.0606602 L19.2426407,17.1213203 C19.8284271,17.7071068 19.8284271,18.6568542 19.2426407,19.2426407 C18.6568542,19.8284271 17.7071068,19.8284271 17.1213203,19.2426407 L16.0606602,18.1819805 Z M3,10.5 L4.5,10.5 C5.32842712,10.5 6,11.1715729 6,12 C6,12.8284271 5.32842712,13.5 4.5,13.5 L3,13.5 C2.17157288,13.5 1.5,12.8284271 1.5,12 C1.5,11.1715729 2.17157288,10.5 3,10.5 Z M12,1.5 C12.8284271,1.5 13.5,2.17157288 13.5,3 L13.5,4.5 C13.5,5.32842712 12.8284271,6 12,6 C11.1715729,6 10.5,5.32842712 10.5,4.5 L10.5,3 C10.5,2.17157288 11.1715729,1.5 12,1.5 Z M12,18 C12.8284271,18 13.5,18.6715729 13.5,19.5 L13.5,21 C13.5,21.8284271 12.8284271,22.5 12,22.5 C11.1715729,22.5 10.5,21.8284271 10.5,21 L10.5,19.5 C10.5,18.6715729 11.1715729,18 12,18 Z M4.81066017,4.81066017 C5.39644661,4.22487373 6.34619408,4.22487373 6.93198052,4.81066017 L7.99264069,5.87132034 C8.57842712,6.45710678 8.57842712,7.40685425 7.99264069,7.99264069 C7.40685425,8.57842712 6.45710678,8.57842712 5.87132034,7.99264069 L4.81066017,6.93198052 C4.22487373,6.34619408 4.22487373,5.39644661 4.81066017,4.81066017 Z M4.81066017,19.2426407 C4.22487373,18.6568542 4.22487373,17.7071068 4.81066017,17.1213203 L5.87132034,16.0606602 C6.45710678,15.4748737 7.40685425,15.4748737 7.99264069,16.0606602 C8.57842712,16.6464466 8.57842712,17.5961941 7.99264069,18.1819805 L6.93198052,19.2426407 C6.34619408,19.8284271 5.39644661,19.8284271 4.81066017,19.2426407 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                                                                </g>
+                                                            </svg> -->
                     <!--end::Svg Icon-->
                     <!-- </span>
-                                                </div> -->
+                                                    </div> -->
                     <!--end::Icon-->
 
                     <!--begin::Info-->
                     <!-- <div class="timeline-desc timeline-desc-light-primary">
-                                                    <span class="font-weight-bolder text-primary"><?php //echo $rd_awal . "-" . $rd_akhir . " WIB";
-                                                    ?></span>
-                                                    <p class="font-weight-normal text-dark-50 pb-2">
-                                                        {{ $r->kegiatan }}
-                                                    </p>
-                                                </div> -->
+                                                        <span class="font-weight-bolder text-primary"><?php //echo $rd_awal . "-" . $rd_akhir . " WIB";
+                                                        ?></span>
+                                                        <p class="font-weight-normal text-dark-50 pb-2">
+                                                            {{ $r->kegiatan }}
+                                                        </p>
+                                                    </div> -->
                     <!--end::Info-->
                     <!-- </div> -->
                     <!--end::Item-->
@@ -437,9 +444,9 @@
                     ?>
                     <!--
     @endforeach
+                                        </div>
                                     </div>
-                                </div>
-                            </div> -->
+                                </div> -->
                     <!--end: Card Body-->
                     <!-- </div> -->
                     <!--end: List Widget 9-->
--- storage/framework/views/5f2c9d6466f263d999e9d7f2bb595aa0b48076c7.php
+++ storage/framework/views/5f2c9d6466f263d999e9d7f2bb595aa0b48076c7.php
@@ -8,21 +8,21 @@
   <meta name="keywords" content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
   <meta name="author" content="elemis">
   <title>MOB FT 2023</title>
-  <link rel="shortcut icon" href="{{ url('./img/mob.png') }}">
-  <link rel="stylesheet" href="{{ url('./assets/css/plugins.css') }}">
-  <link rel="stylesheet" href="{{ url('./assets/css/style.css') }}">
-  <link rel="stylesheet" href="{{ url('./assets/css/colors/sky.css') }}">
-  <link rel="preload" href="{{ url('./assets/css/fonts/urbanist.css') }}" as="style" onload="this.rel='stylesheet'">
-  <link rel="stylesheet" href="{{asset('admin/css/custom.css')}}">
+  <link rel="shortcut icon" href="<?php echo e(url('./img/mob.png')); ?>">
+  <link rel="stylesheet" href="<?php echo e(url('./assets/css/plugins.css')); ?>">
+  <link rel="stylesheet" href="<?php echo e(url('./assets/css/style.css')); ?>">
+  <link rel="stylesheet" href="<?php echo e(url('./assets/css/colors/sky.css')); ?>">
+  <link rel="preload" href="<?php echo e(url('./assets/css/fonts/urbanist.css')); ?>" as="style" onload="this.rel='stylesheet'">
+  <link rel="stylesheet" href="<?php echo e(asset('admin/css/custom.css')); ?>">
   <style>
     .bg-mob {
       background-size: cover; 
-      background-image: url({{asset('assets/mob-assets/background-ungu.jpg')}});
+      background-image: url(<?php echo e(asset('assets/mob-assets/background-ungu.jpg')); ?>);
     }
 
-    @media only screen and (max-width: 768px) {
+    @media  only screen and (max-width: 768px) {
     .bg-mob {
-      background-image: url({{asset('assets/mob-assets/bgmobile.jpg')}});
+      background-image: url(<?php echo e(asset('assets/mob-assets/bgmobile.jpg')); ?>);
       }
     }
   </style>
@@ -30,12 +30,12 @@
 
 <body class="bg-mob">
   <div class="content-wrapper bg-mob">
-		{{-- INI NAVBAR TEMPLATE --}}
+		
 		<header class="wrapper bg-light">
 				<nav class="navbar navbar-expand-lg classic transparent navbar-light">
 					<div class="container flex-lg-row flex-nowrap align-items-center">
 						<div class="navbar-brand w-100">
-							<a href="{{ url('/')}}">
+							<a href="<?php echo e(url('/')); ?>">
                 <a class="main-font" style="font-size: 30px">MOB FT 2023</a>
 							</a>
 						</div>
@@ -43,7 +43,7 @@
 								<div class="offcanvas-body ms-lg-auto d-flex flex-column h-100">
 									<ul class="navbar-nav">
 										<li class="nav-item">
-											<a class="nav-link secondary-font" href="{{route('welcome')}}">Halaman Awal</a>
+											<a class="nav-link secondary-font" href="<?php echo e(route('welcome')); ?>">Halaman Awal</a>
 										</li>
 									</ul>
 									<!-- /.navbar-nav -->
@@ -78,14 +78,21 @@
           <div class="col-xl-10 mx-auto">
             <div class="row gy-10 gx-lg-8 gx-xl-12">
               <div class="col-lg-6 me-auto">
-                <form class="contact-form" method="post" action="{{ route('login') }}">
-                @csrf
+                <form class="contact-form" method="post" action="<?php echo e(route('login')); ?>">
+                <?php echo csrf_field(); ?>
                 <h2 class="ms-2 secondary-font">Login</h2>
                   <div class="row gx-4">
                     <div class="col-md-12">
                       <div class="form-floating mb-4">
-                        <input id="nrp" type="text" class="form-control @error('nrp') is-invalid @enderror" name="nrp" value="{{ old('nrp') }}" placeholder="NRP" required autofocus autocomplete="off">
-                        <label for="nrp">{{ __('NRP') }}</label>
+                        <input id="nrp" type="text" class="form-control <?php $__errorArgs = ['nrp'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="nrp" value="<?php echo e(old('nrp')); ?>" placeholder="NRP" required autofocus autocomplete="off">
+                        <label for="nrp"><?php echo e(__('NRP')); ?></label>
                         <div class="invalid-feedback">
                           NRP atau passwordmu sepertinya masih salah.
                         </div>
@@ -95,7 +102,14 @@
                     <!-- /column -->
                     <div class="col-md-12">
                       <div class="form-floating mb-4">
-                        <input id="password" type="password" name="password" class="form-control @error('password') is-invalid @enderror" required autocomplete="current-password" placeholder="password">
+                        <input id="password" type="password" name="password" class="form-control <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" required autocomplete="current-password" placeholder="password">
                         <label for="password">Password</label>
                         <div class="invalid-feedback">
                           NRP atau Passwordmu sepertinya masih salah.
@@ -110,11 +124,11 @@
                         <label class="form-check-label" for="invalidCheck">
                           Show password
                         </label>
-                      @if (count($errors)>0)
+                      <?php if(count($errors)>0): ?>
                         <div class="invalid-feedback mt-3">
                           <p style="color:#F70000; background-color: #FFE4E2; padding: 1rem; border-radius: 0.5rem">Ada yang salah nih, coba lagi</p>
                         </div>
-                      @endif
+                      <?php endif; ?>
                       </div>
                     </div>
 
@@ -141,8 +155,8 @@
     <!-- /section -->
 
 	</div>
-	<script src="{{ url('./assets/js/plugins.js') }}"></script>
-	<script src="{{ url('./assets/js/theme.js') }}"></script>
+	<script src="<?php echo e(url('./assets/js/plugins.js')); ?>"></script>
+	<script src="<?php echo e(url('./assets/js/theme.js')); ?>"></script>
   <script>
     function showPassword(){
         var pw = document.getElementById("password");
@@ -155,3 +169,4 @@
   </script>
 </body>
 </html>
+<?php /**PATH C:\Github Repository\Web-Utama-MOB-FT-2023\resources\views/auth/login.blade.php ENDPATH**/ ?>
