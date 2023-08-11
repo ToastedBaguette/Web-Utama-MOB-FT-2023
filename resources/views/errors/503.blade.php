576047856f5b4b57b507d9c206f81897a5a196e1
ToastedBaguette <ronyhartonoirawan@gmail.com>
2023-08-11 22:44:52 +0700

    coming soon

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,29 @@
-@extends('errors::minimal')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coming Soon</title>
+  <link rel="shortcut icon" href="{{ url('./img/mob.png') }}">
+  <style>
+    body {
+      display: flex;
+      justify-content: center;
+    }
+
+    img {
+      position: absolute;
+      top: 50%;
+      max-width: 90%;
+      max-height: 90%;
+      transform: translate(0, -50%)
+    }
+  </style>
+</head>
+
+<body style="background-color: #010b49;">
+  <img src="{{ url('./assets/mob-assets/comingsoon.jpg') }}" alt="">
+</body>
+
+</html>
